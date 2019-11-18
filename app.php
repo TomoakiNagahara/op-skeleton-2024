7d8c41bb720e6130ed9ee9e6fd1a4b2053b7078a
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-11-18 18:18:26 +0900

    New: app.php

---
+++ app.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * app-skeleton-2019-nep:/app.php
+ *
+ * @created   2019-11-18
+ * @version   1.0
+ * @package   app-skeleton-2019-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+include('asset/app.php');
