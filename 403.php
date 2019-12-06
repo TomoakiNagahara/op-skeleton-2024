aefcafcd964f15c84be19aafd53eeb1feafde433
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-12-06 17:00:57 +0900

    New: 403.php

---
+++ 403.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * app-skeleton-2019-nep:/403.php
+ *
+ * @creation  2018-11-12
+ * @version   1.0
+ * @package   app-skeleton-2019-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+/* @var $app \OP\UNIT\App */
+
+//	...
+$path = '403_forbidden.phtml';
+
+//	...
+$app->Template($path);
