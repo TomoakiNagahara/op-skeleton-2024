2182abfa9038c009c22e8cac69b17543ba929af2
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-12-12 16:02:25 +0900

    New: asset/config/app.php

---
+++ asset/config/app.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * app-skeleton-2020-nep:/asset/config/10_unit.php
+ *
+ * @created   2019-02-20
+ * @version   1.0
+ * @package   app-skeleton-2019-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+$app = [];
+$app['title']     = 'onepiece-framework app skeleton 2020';
+$app['copyright'] = 'Copyright 2009 All right reserved.';
+
+//	...
+return $app;
+
