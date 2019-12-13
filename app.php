4ea4b3fc0b7a55c26431ddacdf2b93bb6207a027
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2019-12-13 18:55:47 +0900

    Fix: app.php - App launch is move from asset/app.php

--- app.php
+++ app.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * app-skeleton-2019-nep:/app.php
+ * app-skeleton-2020-nep:/app.php
  *
  * @created   2019-11-18
  * @version   1.0
@@ -9,8 +9,45 @@
  * @copyright Tomoaki Nagahara All right reserved.
  */
 
-/** Dispatch.
+ /** namespace
+ *
+ * @created   2019-12-12
+ */
+namespace OP;
+
+/**	Execute time.
+ *
+ * @created
+ * @moved     2019-12-12   asset:/app.php --> app:/app.php
+ */
+$st = microtime(true);
+
+/** Launch onepiece-framework core.
  *
  * @created  2019-11-18
  */
 require('asset/app.php');
+
+/** Launch application.
+ *
+ * @created
+ * @moved     2019-12-12   asset:/app.php --> app:/app.php
+ */
+try {
+	//	Application environment config.
+	require('config.php');
+
+	/* @var $app IF_APP */
+	$app = Unit::Singleton('App');
+
+	//	Launch application.
+	$app->Auto();
+
+	//	Output memory usage.
+	if( Env::isAdmin() and (Env::Mime() === 'text/html') ){
+		$app->Template('app.phtml',['st'=>$st]);
+	};
+
+} catch ( \Throwable $e ){
+	Notice::Set($e);
+}
