72010944b47f62ad5fa3213b30f643266fe66b2d
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-12-27 14:06:32 +0900

    Fix: asset/app.php <-- app.php | Load config.php process.

--- asset/app.php
+++ asset/app.php
@@ -21,6 +21,25 @@ try {
 	//	Bootstrap - Initialize onepiece-framework application.
 	require('bootstrap.php');
 
+	/** Include Application environment config.
+	 *
+	 * @created   2016-11-22   Creation config.php at app-skeleton.
+	 * @updated   2017-??-??   Generate _config.php at app-skeleton2.
+	 * @updated   2019-12-16   Rebirth by app-skeleton-2020-nep.
+	 * @moved     2019-12-27   app-skeleton-2020-nep:/app.php --> asset/app.php
+	 */
+	foreach(['config.php','_config.php'] as $file){
+		//	...
+		$file = __DIR__.'/'.$file;
+
+		//	...
+		if( file_exists($file) ){
+			call_user_func(function($file){
+				include($file);
+			}, $file);
+		}
+	}
+
 } catch ( \Throwable $e ){
 	Notice::Set($e);
 	require('bootstrap/op/failed.phtml');
