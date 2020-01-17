11c95c6a61bf45012abd2dd868bfa92cc9f06dd7
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2020-01-17 12:22:55 +0900

    New: asset/config/i18n.php

---
+++ asset/config/i18n.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * app-skeleton-2020-nep:/asset/config/i18n.php
+ *
+ * @created   2020-01-17
+ * @version   1.0
+ * @package   app-skeleton-2020-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+$url = '//onepiece-framework.com/en:US/i18n/js';
+
+//	...
+return [
+	'url' => $url,
+];
