4c67091ed99fcabee4e5ff7506e058d97963a6ff
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2020-01-20 18:02:41 +0900

    Fix: asset/config.php | Locale is move to asset:/config/locale.php

--- asset/config.php
+++ asset/config.php
@@ -27,6 +27,10 @@ $app_id = substr(md5($seed), 0, 8);
 //	Set App ID.
 Env::AppID($app_id);
 
+/** Locale is move to asset:/config/locale.php
+ *
+ * @moved     2020-01-17
 //	Locale
 Env::Country('jp');
 Env::Language('en');
+*/
