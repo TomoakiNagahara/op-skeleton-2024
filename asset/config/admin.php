ef65b3e18a1ff51cbb33c6d2bef4cee9b2cc2489
Tomoaki imac-2019i9 <tomoaki.nagahara@gmail.com>
2020-07-15 18:40:39 +0900

    New: asset/config/admin.php

---
+++ asset/config/admin.php
@@ -0,0 +1,18 @@
+<?php
+/** op-app-skeleton-2020-nep:/asset/config/admin.php
+ *
+ * @created   2020-07-13
+ * @version   1.0
+ * @package   op-app-skeleton-2020-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** Return config array.
+ *
+ * @return    array        $config
+ */
+return [
+	'e-mail'  => 'info@onepiece-framework.com',
+	'ip-addr' => '127.0.0.1',
+];
