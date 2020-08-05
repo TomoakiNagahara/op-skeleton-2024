0d1c8bfc23429f55d6d5c1e97b53ca4d9bcfcc3d
Tomoaki mbp13r-2015 <tomoaki.nagahara@gmail.com>
2020-08-05 13:32:32 +0900

    Fix: asset > config > google.php | Did not return an array.

---
+++ asset/config/google.php
@@ -0,0 +1,11 @@
+<?php
+/** op-app-skeleton-2020-nep:/asset/config/google.php
+ *
+ * @created   2020-08-05
+ * @version   1.0
+ * @package   op-app-skeleton-2020-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+return [];
