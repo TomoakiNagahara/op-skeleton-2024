7eb9b226467ee35152ca2c51ee6b26c85219eb23
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2020-12-06 17:55:15 +0900

    Fix: asset > gitsubmodule-update.sh | Can change remote name and branch name by arguments.

--- asset/gitsubmodule-update.sh
+++ asset/gitsubmodule-update.sh
@@ -1,5 +1,10 @@
 
+# Branch name
+REMOTE=${1:-origin}
+BRANCH=${2:-2020}
+
+# Update git submodules
 git submodule foreach git fetch
-git submodule foreach git checkout 2020
-git submodule foreach git rebase origin/2020
-git submodule foreach git push origin 2020
+git submodule foreach git checkout         $BRANCH
+git submodule foreach git rebase   $REMOTE/$BRANCH
+git submodule foreach git push     $REMOTE $BRANCH
