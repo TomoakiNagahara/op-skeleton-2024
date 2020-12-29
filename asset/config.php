7a0313e05c91407683e1b3dd4f1de6b0b36c5200
Tomoaki imac-2019i9 <tomoaki.nagahara@gmail.com>
2020-12-29 15:54:09 +0900

    Doc: asset > config.php | @package

--- asset/config.php
+++ asset/config.php
@@ -1,13 +1,12 @@
 <?php
-/**
- * app-skeleton-2020-nep:/config.php
+/** op-app-skeleton-2020-nep:/config.php
  *
  * @created   2019-04-09   asset:/config/app.php
  * @moved     2019-11-20   asset:/config/app.php --> asset:/config/env.php
  * @moved     2019-12-12   asset:/config/env.php --> asset:/config.php
  * @moved     2019-12-12   asset:/config.php     --> app:/config.php
  * @version   1.0
- * @package   app-skeleton-2019-nep
+ * @package   op-app-skeleton-2020-nep
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
