89b90e2128773777755a47453b17f55fbf4db990
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-08-31 22:14:17 +0900

    Fix: asset > config > admin.php | Added usage information.

--- asset/config/admin.php
+++ asset/config/admin.php
@@ -14,9 +14,12 @@
  */
 $config = [
 /*
-	OP\Env::_ADMIN_IP_   => '127.0.0.1',
+	//	Access with this IP address will enable developer mode.
+	OP\Env::_ADMIN_IP_   => '153.127.64.66',
+	//	An error message will be sent to this email address.
 	OP\Env::_ADMIN_MAIL_ => 'info@onepiece-framework.com',
-	OP\Env::_MAIL_FROM_  => 'noreply@onepiece-framework.com',
+	//	This email address will be the from of the admin email.
+	OP\Env::_ADMIN_FROM_ => 'noreply@onepiece-framework.com',
 */
 ];
 
