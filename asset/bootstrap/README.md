9cc1253460481dc2921a0879b49dc0e7a48af9e8
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-10-02 21:54:56 +0900

    New: asset > bootstrap | html_head.phtml, html_foot.phtml, style.css

--- asset/bootstrap/README.md
+++ asset/bootstrap/README.md
@@ -1,2 +1,4 @@
 Bootstrap helper module of onepiece-framework
 ===
+
+  This under files is called from asset:/bootstrap.php.
--- asset/bootstrap/op/rewrite.php
+++ asset/bootstrap/op/rewrite.php
@@ -8,49 +8,7 @@
  * @copyright Tomoaki Nagahara All right reserved.
  */
 ?>
-<!doctype html>
-<html>
-	<head>
-		<title>Boot to the onepiece-framework</title>
-		<style>
-
-		h1 {
-			font-size: 1.5vw;
-		}
-
-		li {
-			margin-bottom: 1em;
-		}
-
-		div[role="code"] {
-			border: 1px solid #9f9f9f;
-			margin:  1em 0;
-			padding: 0.7em 1em;
-			font-family: monospace;
-		}
-
-		pre {
-			border: 1px solid #9f9f9f;
-			border-radius: 0.25em;
-			padding: 0.5em 0.5em;
-		}
-
-		code {
-
-		}
-
-		pre, code {
-			font-size:   14px;
-			line-height:  1.2;
-			font-family: Consolas, 'Courier New', Courier, Monaco, monospace;
-		}
-		</style>
-	</head>
-	<body>
-		<header>
-			Boot to the onepiece-framework
-		</header>
-		<hr/>
+<?php include(__DIR__.'/../html_head.phtml') ?>
 		<div data-i18n="true" data-lang="en">
 		<?php
 		//	...
@@ -65,11 +23,4 @@
 		}
 		?>
 		</div>
-		<hr/>
-		<footer>
-			Copyright 2006 onepiece-framework all right reserved
-		</footer>
-	</body>
-</html>
-<?php
-exit();
+<?php include(__DIR__.'/../html_foot.phtml') ?>
