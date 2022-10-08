654b4d6e81a5eb3f7a1703e83cf9519c9fb00f85
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-10-08 23:37:57 +0900

    New: asset > config > core.php, router.php

---
+++ asset/config/core.php
@@ -0,0 +1,27 @@
+<?php
+/** skeleton:/asset/config/core.php
+ *
+ * @created   2022-10-04
+ * @version   1.0
+ * @package   skeleton
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** Return config array.
+ *
+ */
+$config = [
+	'dir' => [
+		'core'     => 'asset:/core/',
+		'unit'     => 'asset:/nunit/',
+		'layout'   => 'asset:/layout/',
+		'develop'  => 'asset:/develops/',
+		'template' => 'asset:/template/',
+		'config'   => 'asset:/config/',
+		'cache'    => 'asset:/cache/',
+	],
+];
+
+//	...
+return $config;
---
+++ asset/config/router.php
@@ -0,0 +1,18 @@
+<?php
+/** skeleton:/asset/config/router.php
+ *
+ * @created   2022-10-04
+ * @version   1.0
+ * @package   skeleton
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** Return config array.
+ *
+ */
+$config = [
+];
+
+//	...
+return $config;
