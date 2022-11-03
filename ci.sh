ee8779f393babfb763f3675f101d82433b4b0fea
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-11-03 18:58:35 +0900

    CI: ci.sh

---
+++ ci.sh
@@ -0,0 +1,12 @@
+
+ ## op-core:/ci.sh
+ #
+ # Call from git pre-push
+ #
+ # @created   2022-10-31
+ # @version   1.0
+ # @package   op-core
+ # @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ # @copyright Tomoaki Nagahara All right reserved.
+
+php81 ci.php display=0
