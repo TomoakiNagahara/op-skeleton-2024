8c63f6af938dc5efb988395d751bfd0aa648066c
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-11-03 18:43:15 +0900

    New: asset > config > env.php

---
+++ asset/config/env.php
@@ -0,0 +1,19 @@
+<?php
+/** op-app-skeleton-2020-nep:/asset/config/env.php
+ *
+ * @created   2022-10-18
+ * @version   1.0
+ * @package   op-app-skeleton-2020-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** Return config array.
+ *
+ * @return    array        $config
+ */
+$config = [
+];
+
+//	...
+return $config;
