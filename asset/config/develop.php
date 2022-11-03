c291ebd9444033b26476b239485d663f8648ecd3
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-11-03 18:39:22 +0900

    New: asset > config > develop.php

---
+++ asset/config/develop.php
@@ -0,0 +1,19 @@
+<?php
+/** skeleton:/asset/config/develop.php
+ *
+ * @created   2022-11-01
+ * @version   1.0
+ * @package   skeleton
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** Return config array.
+ *
+ */
+$config = [
+	'layout' => 'white',
+];
+
+//	...
+return $config;
