113ca78b995d42bee5fdffd615b2c3d1dc8ae7ed
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-11-08 15:19:21 +0900

    2022: asset > bootstrap.php | Disable closure.

--- asset/bootstrap.php
+++ asset/bootstrap.php
@@ -15,7 +15,7 @@
 namespace OP;
 
 //	...
-call_user_func(function(){
+//call_user_func(function(){
 	try{
 		//	Bootstrap the onepiece-framework's core.
 		$path = __DIR__.'/core/Bootstrap.php';
@@ -96,4 +96,4 @@ call_user_func(function(){
 		$message = $e->getMessage();
 		exit("$file #$line, $message");
 	};
-});
+//});
