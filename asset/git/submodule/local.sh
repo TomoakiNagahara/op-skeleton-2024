a886419ad7def629ac30bcc1b4fd513016d1bead
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-11-15 19:24:44 +0900

    Fix: asset > git > submodule > local.sh | Change to full path.

--- asset/git/submodule/local.sh
+++ asset/git/submodule/local.sh
@@ -1,6 +1,9 @@
 
+# sudo port install realpath
+HOME=$(realpath ~/)
+
 # Replace
-sed -i -e "s|https://github.com/onepiece-framework/|~/repo/op/|g" .gitmodules
+sed -i -e "s|https://github.com/onepiece-framework/|${HOME}/repo/op/|g" .gitmodules
 sed -i -e "s|/unit-|/unit/|g" .gitmodules
 sed -i -e "s|/module-|/module/|g" .gitmodules
 sed -i -e "s|/layout-|/layout/|g" .gitmodules
