92d0156ea3b9af28acb900e104735a33f0a86749
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-11-15 18:02:30 +0900

    New: asset > config > op.php | _BRANCH_

---
+++ asset/config/op.php
@@ -0,0 +1,20 @@
+<?php
+/** op-app-skeleton-2020-nep:/asset/config/op.php
+ *
+ * @created   2022-11-15
+ * @version   1.0
+ * @package   op-app-skeleton-2020-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	onepiece-framework settings.
+define('_BRANCH_', '2022', true);
+
+//	...
+return [];
