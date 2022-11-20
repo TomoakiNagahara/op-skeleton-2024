888c7dfb40f2997433d7784309c667994533ca5a
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-11-20 16:45:51 +0900

    Fix: asset > git > submodule > local.sh | op-core.git --> core.git

--- asset/git/submodule/local.sh
+++ asset/git/submodule/local.sh
@@ -4,12 +4,13 @@ HOME=$(realpath ~/)
 
 # Replace
 sed -i -e "s|https://github.com/onepiece-framework/|${HOME}/repo/op/|g" .gitmodules
-sed -i -e "s|/unit-|/unit/|g" .gitmodules
-sed -i -e "s|/module-|/module/|g" .gitmodules
-sed -i -e "s|/layout-|/layout/|g" .gitmodules
-sed -i -e "s|/op-unit-|/unit/|g" .gitmodules
-sed -i -e "s|/op-module-|/module/|g" .gitmodules
-sed -i -e "s|/op-layout-|/layout/|g" .gitmodules
+sed -i -e "s|/unit-|/unit/|g"          .gitmodules
+sed -i -e "s|/module-|/module/|g"      .gitmodules
+sed -i -e "s|/layout-|/layout/|g"      .gitmodules
+sed -i -e "s|/op-core.git|/core.git|g" .gitmodules
+sed -i -e "s|/op-unit-|/unit/|g"       .gitmodules
+sed -i -e "s|/op-module-|/module/|g"   .gitmodules
+sed -i -e "s|/op-layout-|/layout/|g"   .gitmodules
 sed -i -e "s|/op-webpack-|/webpack/|g" .gitmodules
 
 # Sync
