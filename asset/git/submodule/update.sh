2ca9fd4d6e7ef0fc676e81a86e02d16e70faec62
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-11-20 11:21:12 +0900

    Fix: asset > git > submodule > update.sh | save -> rebase -> pop

--- asset/git/submodule/update.sh
+++ asset/git/submodule/update.sh
@@ -2,15 +2,12 @@
 # Branch name
 REMOTE=${1:-origin}
 BRANCH=${2:-2022}
-#CURENT=OP
 
 # Update git submodules
-#git submodule foreach git branch           $CURENT
 git submodule foreach git add .
 git submodule foreach git stash save
-git submodule foreach git fetch    $REMOTE
 git submodule foreach git checkout         $BRANCH
+git submodule foreach git fetch    $REMOTE
 git submodule foreach git rebase   $REMOTE/$BRANCH
-#git submodule foreach git checkout         $CURENT
-#git submodule foreach git stash pop
-#git submodule foreach git reset
+git submodule foreach git stash pop
+git submodule foreach git reset
