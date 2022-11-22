2b9fa9448d895603e234faf74d6a57c440f2bee8
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-11-22 22:19:24 +0900

    CI: ci.sh | Stash uncommitted files.

--- ci.sh
+++ ci.sh
@@ -7,14 +7,26 @@
  # @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  # @copyright Tomoaki Nagahara All right reserved.
 
-# git stash save
-# git submodule foreach git stash save
+# Skeleton
+git add .
+git stash save
+
+# Submodules
+git submodule foreach git add .
+git submodule foreach git stash save
 
 # CI
 php ci.php display=0
+
+# Skeleton
+git stash pop
+git reset
+
+# Submodules
+git submodule foreach git stash pop
+git submodule foreach git reset
+
+# Result
 if [ $? -ne 0 ]; then
   exit 1
 fi
-
-# git stash pop
-# git submodule foreach git stash pop
