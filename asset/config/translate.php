78b20f4e3121497d79c44d9267b4d6dd51e3f7a7
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-12-30 15:52:07 +0900

    Fix: asset > config > translate.php

---
+++ asset/config/translate.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * app-skeleton-2020-nep:/asset/config/translate.php
+ *
+ * @created   2022-12-30
+ * @version   1.0
+ * @package   app-skeleton-2020-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+ /** namespace
+ *
+ */
+namespace OP;
+
+/** Return config array.
+ *
+ * @return    array        $config
+ */
+return [
+	'execute' => false,
+];
