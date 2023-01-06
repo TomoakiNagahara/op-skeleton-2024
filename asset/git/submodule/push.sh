60c45483d37dbaa352f3b6a50ead0fe2110fccd0
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-06 19:35:09 +0900

    Fix: asset > git > submodule > push.sh | Remote argument added.

--- asset/git/submodule/push.sh
+++ asset/git/submodule/push.sh
@@ -1,12 +1,18 @@
 
+# Remote name
+REMOTE=${1}
 # Branch name
-BRANCH=${1}
+BRANCH=${2}
 
 # Check argument.
+if [ -z $REMOTE ]; then
+  echo 'Empty remote name. --> sh asset/git/submodule/push.sh origin 2022'
+  exit 1
+fi
 if [ -z "$BRANCH" ]; then
-  echo 'Empty branch name. --> sh asset/git/submodule/push.sh 2022'
+  echo 'Empty branch name. --> sh asset/git/submodule/push.sh origin 2022'
   exit 1
 fi
 
 # Push
-git submodule foreach git push origin $BRANCH
+git submodule foreach git push $REMOTE $BRANCH
