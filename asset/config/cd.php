09cfd93328f8ffb92bd3c534519018330ffebfb6
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-15 16:34:45 +0900

    CD: asset > config > cd.php

---
+++ asset/config/cd.php
@@ -0,0 +1,23 @@
+<?php
+/** op-app-skeleton-2020-nep:/asset/config/cd.php
+ *
+ * @created   2023-01-15
+ * @version   1.0
+ * @package   op-app-skeleton-2020-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** Return config array.
+ *
+ * @return    array        $config
+ */
+$config = [
+	'execute' => true,
+	'github' => [
+		'secret' => 'my secret key',
+	],
+];
+
+//	...
+return $config;
