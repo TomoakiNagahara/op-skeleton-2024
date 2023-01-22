05602b64ec625e5becb008b97b87c1d819b15d3c
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-22 20:46:46 +0900

    New: css > index.php

---
+++ css/index.php
@@ -0,0 +1,40 @@
+<?php
+/** op-app-skeleton-2020-nep:/css/index.php
+ *
+ * @created   2023-01-22
+ * @version   1.0
+ * @package   op-app-skeleton-2020-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+//echo "Could load index.php\n*/\n";
+
+//	...
+foreach( glob('*.css') as $file ){
+	//	...
+	if( $file === 'index.css' ){
+		continue;
+	}
+
+	//	...
+	if( $file[0] === '.' or $file[0] === '_' ){
+		continue;
+	}
+
+	//	...
+	echo OP::Template($file);
+}
+
+//echo "\n/*\nFinish index.php\n";
