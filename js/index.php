12d8801017cde95bd3593cbc0bae7798ff1ecf4d
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-22 20:45:58 +0900

    New: js > index.php

--- js/index.php
+++ js/index.php
@@ -1,2 +1,35 @@
+<?php
+/** op-app-skeleton-2020-nep:/js/index.php
+ *
+ * @created   2023-01-22
+ * @version   1.0
+ * @package   op-app-skeleton-2020-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
 
-/* index.js */
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+foreach( glob('*.js') as $file ){
+	//	...
+	if( $file === 'index.js' ){
+		continue;
+	}
+
+	//	...
+	if( $file[0] === '.' or $file[0] === '_' ){
+		continue;
+	}
+
+	//	...
+	echo OP::Template($file);
+}
