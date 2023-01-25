46f51de425dcb5e8bbc0a5fb6af6b844ba96a1ad
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-25 18:55:11 +0900

    Fix: js > OP_Translate_Language.js

--- js/OP_Translate_Language.js
+++ js/OP_Translate_Language.js
@@ -1,5 +1,5 @@
 
-/** op-app-skeleton-2020-nep:/translate-language.js
+/** op-app-skeleton-2020-nep:/OP_Translate_Language.js
  *
  * @created   2023-01-22
  * @version   1.0
@@ -8,22 +8,64 @@
  * @copyright Tomoaki Nagahara All right reserved.
  */
 
+/* <?php if( OP()->Config('translate')['execute'] ?? null ): ?> */
+
 //	...
 document.addEventListener('DOMContentLoaded', () => {
 	//	...
-	fetch('https://onepiece-framework.com/api/i18n/language?target=language')
-		.then((response) => response.json())
-		.then((json) => Display(json));
+	let item_language_list = 'tranlate_language_list';
+	let item_language_code = 'tranlate_language_code';
+
+	//	...
+	let json = localStorage.getItem(item_language_list);
+	if( json ){
+		json = JSON.parse(json);
+		Display(json);
+	}else{
+		Fetch();
+	}
+
+	//	...
+	function Fetch(){
+		//	...
+		let URL = 'https://onepiece-framework.com/api/i18n/language?target=language';
+		D(`Fetch: ${URL}`);
+		fetch(URL)
+			.then((response) => response.json())
+			.then((json) => {
+				//	...
+				if(!json['status'] ){
+					console.error('status is not true', json);
+					return;
+				}
+				//	...
+				if( json['errors'] ){
+					console.error('has errors', json);
+					return;
+				}
+				//	...
+				if(!json['result'] ){
+					console.error('result is empty', json);
+					return;
+				}
+				//	...
+				if(!json['result']['language'] ){
+					console.error('result is empty', json);
+					return;
+				}
+				//	...
+				localStorage.setItem(item_language_list, JSON.stringify(json));
+				Display(json);
+			});
+	}
 
 	//	...
 	function Display(json){
-		console.log(json);
-
 		//	...
 		let name = "<?php echo OP()->Config('translate')['language-area-id'] ?? 'null'; ?>";
 		let area = document.querySelector(`#${name}`);
 		if(!area ){
-			console.error(`Does not found '#${name}'`);
+			D(`Does not found '#${name}'`);
 			return;
 		}
 
@@ -44,9 +86,23 @@ document.addEventListener('DOMContentLoaded', () => {
 				a.title = english;
 			let li   = document.createElement('li');
 				li.appendChild(a);
+				li.href = key;
+				li.onclick = onClick;
 
 			//	...
 			area.appendChild(li);
 		}
-	}
+	};
+
+	//	...
+	function onClick(){
+		//	...
+		language_code = this.href;
+		localStorage.setItem(item_language_code, language_code);
+
+		//	...
+		return false;
+	};
 });
+
+/* <?php endif; ?> */
