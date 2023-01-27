43ec0dd9229ccc1f0c948937145e09357871a785
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-27 18:19:05 +0900

    New: asset > config > execute.php

---
+++ asset/config/execute.php
@@ -0,0 +1,21 @@
+<?php
+/** app-skeleton-2020-nep:/asset/config/execute.php
+ *
+ * @created   2023-01-26
+ * @version   1.0
+ * @package   app-skeleton-2020-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** Return config array.
+ *
+ * @return    array        $config
+ */
+return [
+	'layout'    => true,
+	'translate' => true,
+	'etag'      => false,
+	'markdown'  => true,
+	'highlight' => true,
+];
