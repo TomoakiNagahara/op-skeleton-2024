d073bc7dc425f059ef44d7e36d2cb5b4a7028712
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-29 13:50:47 +0900

    Fix: asset > git > submodule > unit > add.sh | Help message.

--- asset/git/submodule/unit/add.sh
+++ asset/git/submodule/unit/add.sh
@@ -14,6 +14,12 @@ BRANCH=${2:-2022}
 URL=https://github.com/onepiece-framework/op-unit-$UNIT.git
 PATH=asset/unit/$UNIT
 
+# Validation
+if [ -z "$UNIT" ]; then
+  echo 'Empty unit name. --> sh asset/git/submodule/unit/add.sh {unit_name}'
+  exit 1
+fi
+
 # Add git submodule
 git submodule add      $URL $PATH
 git submodule init
