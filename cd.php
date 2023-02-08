79d31be05ad0828d8ad17a36ea4cb0de52210c0e
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-02-08 22:01:33 +0900

    New: cd.php

---
+++ cd.php
@@ -0,0 +1,58 @@
+<?php
+/** op-app-skeleton-2020-nep:/cd.php
+ *
+ # <pre>
+ # How to use: `php cd.php upstream master`
+ # </pre>
+ #
+ * @created    2023-02-05
+ * @version    1.0
+ * @package    op-app-skeleton-2020-nep
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/**	Execute time.
+ *
+ */
+define('_OP_APP_START_', microtime(true));
+
+//	...
+$exit = 0;
+
+//	...
+try {
+	//	...
+	include('asset/init.php');
+
+	//	...
+	OP::Unit('CD')->Auto();
+
+}catch( \Throwable $e ){
+	//	...
+	$message = $e->getMessage();
+
+	//	...
+	echo "\n\n";
+	echo "Exception: {$message}\n\n";
+	foreach( $e->getTrace() as $trace){
+		echo ' * '.OP::DebugBacktraceToString($trace)."\n";
+	}
+	echo "\n";
+
+	//	...
+	$exit = __LINE__;
+}
+
+//	exit
+exit($exit);
