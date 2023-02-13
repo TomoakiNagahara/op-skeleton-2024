bf206db75f7558f583b74541c08e5bc6d16a09e7
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-02-13 11:55:27 +0900

    Chg: ci.sh | v2.0

--- ci.sh
+++ ci.sh
@@ -1,41 +1,52 @@
 
  ## op-app-skeleton-2020-nep:/ci.sh
  #
- # <pre>
- # How to use: `sh ci.sh`
+ # Call from git pre-push
  #
- # This file does a git stash save.
- # Uncommitted files are not inspect.
- # </pre>
- #
- # @created   2022-10-31
- # @version   1.0
- # @package   op-app-skeleton-2020-nep
- # @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
- # @copyright Tomoaki Nagahara All right reserved.
+ # @created    2022-10-31
+ # @rewrite    2023-02-09
+ # @version    2.0
+ # @package    op-core
+ # @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ # @copyright  Tomoaki Nagahara All right reserved.
 
-# Skeleton
-git add .
-git stash save
+# Get command
+COMMAND=$(ps -ocommand= -p $PPID)
 
-# Submodules
-git submodule foreach git add .
-git submodule foreach git stash save
+# Parse
+ARRAY=(${COMMAND//,/ })
+REMOTE=${ARRAY[2]}
+BRANCH=${ARRAY[3]}
 
-# CI
-php ci.php display=0
-# Result
-if [ $? -ne 0 ]; then
-  EXIT=1
+# Get current branch name
+#BRANCH=`git rev-parse --abbrev-ref HEAD`
+if [ ! $BRANCH ]; then
+  echo "Empty branch name."
+  exit 1
 fi
 
-# Skeleton
-git stash pop
-git reset
+# Set CI saved commit id file name
+CI_FILE=".ci_commit_id_"$BRANCH
+#echo $CI_FILE
 
-# Submodules
-git submodule foreach git stash pop
-git submodule foreach git reset
+# Check if file exists
+if [ ! -f $CI_FILE ]; then
+  echo "Does not file exists. ($CI_FILE)"
+  exit 1
+fi
 
-# Exit
-exit $EXIT
+# Get commit id
+CI_COMMIT_ID=`cat $CI_FILE`
+#echo $CI_COMMIT_ID
+
+# Get correct commit id
+COMMIT_ID=`git rev-parse $BRANCH`
+#echo $COMMIT_ID
+
+#
+if [ $COMMIT_ID != $CI_COMMIT_ID ]; then
+  echo "Unmatch commit id"
+  echo $COMMIT_ID branch=$BRANCH
+  echo $CI_COMMIT_ID $CI_FILE
+  exit 1
+fi
