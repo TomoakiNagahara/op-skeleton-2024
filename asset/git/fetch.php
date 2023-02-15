c68d08e60963e9962f1eecc4779679711294e328
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-02-15 20:07:56 +0900

    New: asset > git > fetch.php

---
+++ asset/git/fetch.php
@@ -0,0 +1,63 @@
+<?php
+/** op-app-skeleton-2020-nep:/asset/git/fetch.php
+ *
+ * <pre>
+ * ```sh
+ * php git.php asset/git/fetch.php remote=orign display=1 debug=0
+ * ```
+ * </pre>
+ *
+ * @created    2023-02-15
+ * @version    1.0
+ * @package    op-app-skeleton-2020-nep
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$display = OP::Request('display') ?? 1;
+$debug   = OP::Request('debug')   ?? 0;
+
+/* @var $git UNIT\Git */
+$git = OP::Unit('Git');
+
+//	...
+$configs = $git->SubmoduleConfig();
+if( $debug ){
+	D($configs);
+}
+
+//	...
+foreach( $configs as $config ){
+	//	...
+	$meta = 'git:/'.$config['path'];
+	$path = OP::MetaPath($meta);
+	if(!chdir($path) ){
+		throw new \Exception("chdir was failed. ({$meta}, {$path})");
+	}
+	if( $display ){ D("Change Directory: {$meta}"); }
+
+	//	...
+	$git->Fetch();
+}
+
+//	...
+$meta = 'git:/';
+$path = OP::MetaPath($meta);
+if(!chdir($path) ){
+	throw new \Exception("chdir was failed. ({$meta}, {$path})");
+}
+if( $display ){ D("Change Directory: {$meta}"); }
+
+//	...
+$git->Fetch();
