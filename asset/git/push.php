edf24a93c405de21a4e753c27a4891f5dcaa29bc
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-02-17 18:45:05 +0900

    New: asset > git > push.php

---
+++ asset/git/push.php
@@ -0,0 +1,62 @@
+<?php
+/** op-app-skeleton-2020-nep:/asset/git/fetch.php
+ *
+ * <pre>
+ * ```sh
+ * php git.php asset/git/fetch.php remote=orign display=1 debug=0
+ * ```
+ * </pre>
+ *
+ * @created    2023-02-15
+ * @version    1.0
+ * @package    op-app-skeleton-2020-nep
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$display = OP::Request('display') ?? 1;
+$remote  = OP::Request('remote')  ?? 'origin';
+$branch  = OP::Request('branch')  ?? _OP_APP_BRANCH_;
+$force   = OP::Request('force')    ? true: false;
+
+/* @var $git UNIT\Git */
+$git = OP::Unit('Git');
+
+//	...
+$configs = $git->SubmoduleConfig();
+
+//	...
+foreach( $configs as $config ){
+	//	...
+	$meta = 'git:/'.$config['path'];
+	$path = OP::MetaPath($meta);
+	if(!chdir($path) ){
+		throw new \Exception("chdir was failed. ({$meta}, {$path})");
+	}
+	if( $display ){ D("Change Directory: {$meta}"); }
+
+	//	...
+	$git->Push($remote, $branch, $force);
+}
+
+//	...
+$meta = 'git:/';
+$path = OP::MetaPath($meta);
+if(!chdir($path) ){
+	throw new \Exception("chdir was failed. ({$meta}, {$path})");
+}
+if( $display ){ D("Change Directory: {$meta}"); }
+
+//	...
+$git->Push($remote, $branch, $force);
