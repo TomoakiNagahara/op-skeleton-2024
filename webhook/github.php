0def297a94bf08fa056b20a2ac6d117c24d2e65e
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-03-03 20:43:16 +0900

    Fix: webhook > github.php | v2.0

--- webhook/github.php
+++ webhook/github.php
@@ -1,11 +1,11 @@
 <?php
-/** op-app-skeleton-2020-nep:/cd.php
+/** op-app-skeleton-2020-nep:/webhook/github.com
  *
- * @created   2022-11-21
- * @version   1.0
- * @package   op-app-skeleton-2020-nep
- * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
- * @copyright Tomoaki Nagahara All right reserved.
+ * @created    2022-11-21
+ * @version    2.0
+ * @package    op-app-skeleton-2020-nep
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
  */
 
 /** Declare strict
@@ -17,21 +17,6 @@ declare(strict_types=1);
  *
  */
 namespace OP;
-
-/**	Execute time.
- *
- */
-if(!$_SERVER['REQUEST_TIME_FLOAT'] ?? null ){
-	$_SERVER['REQUEST_TIME_FLOAT'] = microtime(true);
-}
-
-/*
-//	MIME
-header('Content-Type: text/plain; charset=UTF-8');
-
-//	...
-require_once('asset/core/include/json.php');
-*/
 
 //	MIME
 Env::MIME('text/plain');
@@ -51,6 +36,10 @@ if( true ){
 	}
 
 	//	...
+	if( Env::isAdmin() ){
+		//	OK
+		echo "isAdmin\n";
+	}else
 	if( $signature !== $hash ){
 		$message = "Signature is unmatch.";
 		echo $message;
@@ -58,41 +47,27 @@ if( true ){
 	}
 
 	//	...
+	if( $comand = OP::Config('cd')['github'][$action] ?? null ){
+		D($comand);
+	}else{
+		D("Empty: config:/cd.php > github > {$action}");
+	}
+
+	/*
+	//	...
 	switch( $action ){
 		//	GitHub action
 		case 'completed':
-			if( $cd = $_SERVER['_OP_CD_'] ?? null ){
-				echo `{$cd}`;
-			}else{
-				OP::Notice('GitHub WebHook: $_SERVER[_OP_CD_] is empty.');
-			}
 			break;
 
 		//	op-cd
 		case 'update':
-		//	`sh asset/git/submodule/update.sh`;
 			break;
 
 		default:
 	}
+	*/
 }
-
-/*
-//	...
-if( null ){
-	//	...
-	if(!apcu_store('account', $account) ){
-		echo "Store account is fail. ({$account})\n";
-		exit(__LINE__);
-	}
-}else{
-	//	...
-	if(!$account = apcu_fetch('account') ){
-		echo "Fetch account is fail.\n";
-		exit(__LINE__);
-	}
-}
-*/
 
 //	...
 echo $account;
