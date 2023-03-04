b08b8e3901cda6614e3e55562f558f2744172a7a
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-03-04 22:17:08 +0900

    New: asset > config > webhook.php

---
+++ asset/config/webhook.php
@@ -0,0 +1,28 @@
+<?php
+/** op-app-skeleton-2020:/asset/config/webhook.php
+ *
+ * @created    2023-03-07
+ * @version    1.0
+ * @package    op-app-skeleton-2020
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Return config array.
+ *
+ * @return    array        $config
+ */
+return [
+	'github' => [
+		'develop'   => [
+			'whoami',
+			'echo $PATH',
+			'ls -la',
+		],
+		'completed' => [
+			"sudo -u developer /opt/local/bin/php82 --version",
+			"sudo -u developer /opt/local/bin/php82 ci.php display=1 debug=1",
+			"sudo -u developer /opt/local/bin/php82 cd.php display=1 debug=1 remote=upstream",
+		],
+	],
+];
