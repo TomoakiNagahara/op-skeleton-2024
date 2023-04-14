f4372522d05358b1d962782f492a2ab3786f6a84
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-04-14 19:26:20 +0900

    Fix: ci.sh | v2.2.0

--- ci.sh
+++ ci.sh
@@ -1,11 +1,13 @@
 
- ## op-app-skeleton-2020-nep:/ci.sh
+ ## op-core:/ci.sh
  #
  # Call from git pre-push
  #
  # @created    2022-10-31
- # @rewrite    2023-02-09
- # @version    2.1
+ # @updated    2023-02-09 v2.0
+ # @updated    2023-02-14 v2.1
+ # @updated    2023-04-13 v2.2
+ # @version    2.2.0
  # @package    op-core
  # @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  # @copyright  Tomoaki Nagahara All right reserved.
@@ -22,8 +24,15 @@ PHP=`php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;"`
 # Get current branch name
 #BRANCH=`git rev-parse --abbrev-ref HEAD`
 if [ ! $BRANCH ]; then
-  echo "Empty branch name."
+  echo "ci.sh: Empty branch name."
   exit 1
+fi
+
+# Check if branch name
+if [[  $BRANCH =~ ^php([0-9]{2})$ ]]; then
+    PHP=${BASH_REMATCH[1]}
+else
+    PHP=`php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;"`
 fi
 
 # Set CI saved commit id file name
@@ -32,7 +41,7 @@ CI_FILE=".ci_commit_id_"$BRANCH"_php"$PHP
 
 # Check if file exists
 if [ ! -f $CI_FILE ]; then
-  echo "Does not file exists. ($CI_FILE)"
+  echo "ci.sh: Does not file exists. ($CI_FILE)"
   exit 1
 fi
 
