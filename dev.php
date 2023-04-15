85b6beb626b4222a9e13ad7ad3619ae21faf3f41
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-04-15 18:20:52 +0900

    Fix: dev.php | Save file path is generate from config.

--- dev.php
+++ dev.php
@@ -14,73 +14,128 @@ if(!extension_loaded('tideways_xhprof') ){
     return;
 }
 
-//  Start
-tideways_xhprof_enable(/* TIDEWAYS_XHPROF_FLAGS_CPU | TIDEWAYS_XHPROF_FLAGS_MEMORY */);
+/** Start
+ *
+ * <pre>
+ * XHPROF_FLAGS_NO_BUILTINS (int)
+ *     Used to skip all built-in (internal) functions.
+ *
+ * XHPROF_FLAGS_CPU (int)
+ *     Used to add CPU profiling information to the output.
+ *
+ * XHPROF_FLAGS_MEMORY (int)
+ *     Used to add memory profiling information to the output.
+ * </pre>
+ *
+ * @see https://www.php.net/manual/en/xhprof.constants.php
+ */
+tideways_xhprof_enable( TIDEWAYS_XHPROF_FLAGS_CPU | TIDEWAYS_XHPROF_FLAGS_MEMORY );
 
 //  App
 include('app.php');
 
 //  Finish
-$data = tideways_xhprof_disable();
+$prof = tideways_xhprof_disable();
 
-//  Save
+//  Config
+$config      = OP()->Config('xhprof');
+
+//  ...
+$whoami  = trim(`whoami`);
 $version = PHP_MAJOR_VERSION.PHP_MINOR_VERSION;
-$time    = date('H-i-s').'-'.explode('.', $_SERVER["REQUEST_TIME_FLOAT"])[1];
-$ext     = true ? 'xhprof': 'json'; // extension
-$file    = "php{$version}_{$time}.{$ext}";
-if( false ){
-    $path    = sys_get_temp_dir() . "/" . uniqid() . ".yourapp.xhprof";
-    $dir     = '/www/_caches/php/xhprof/test/';
-    $time    = date('H-i-s').'-'.explode('.', $_SERVER["REQUEST_TIME_FLOAT"])[1];
-}else{
-    $whoami  = trim(`whoami`);
-    $year    = date('Y');
-    $month   = date('m');
-    $day     = date('d');
-    $root    = "/www/_caches/php/xhprof/";
-    $dir     = "{$root}/{$whoami}/{$year}/{$month}/{$day}/";
-    $path    = "{$dir}/{$file}";
+$host    = $_SERVER['SERVER_NAME'] ?? 'cli';
+$host    = str_replace('.', '-', $host);
+$date    = date('Y-m-d');
+$time    = date('H-i-s');
+$mtime   = $_SERVER["REQUEST_TIME_FLOAT"] ?? '.'.uniqid();
+$mtime   = explode('.', $mtime)[1];
+$url     = OP()->Router()->EndPoint();
+$url     = dirname( $url );
+$url     = str_replace($_SERVER['DOCUMENT_ROOT'], '', $url);
+$url     = str_replace('/', '-', trim($url,'/'));
+$args    = OP()->Router()->Args();
+$args    = join('-', $args);
+$temp    = $whoami.$version.$date.$time;
 
-    //  ...
-    if( \OP\Env::isHttp() ){
-        $host    = $_SERVER['SERVER_NAME'];
-        $path    = "{$dir}/{$host}_{$file}";
-        $endpoint= dirname( OP()->Router()->EndPoint() );
-        if( strpos($endpoint, $_SERVER['DOCUMENT_ROOT']) !== 0 ){
-            Notice("Unmatch end-point: {$endpoint}, {$_SERVER['DOCUMENT_ROOT']}");
-        }else{
-            $doc = rtrim($_SERVER['DOCUMENT_ROOT'], '/');
-            $len = strlen($doc);
-            $url = substr($endpoint, $len);
-            $link= "{$root}/{$whoami}/{$host}/{$url}/";
-        }
-    }else{
+//  Get root path
+$root_path = $config['root_path'];
+
+//  Generate file path
+$temp = [];
+foreach( $config['file_path'] as $key => $value ){
+    if( $value ){
+        $temp[] = ${$key};
     }
+}
+$file_path = join('_', $temp);
+
+//  Generate file name
+$temp = [];
+foreach( $config['file_name'] as $key => $value ){
+    if( $value ){
+        $temp[] = ${$key};
+    }
+}
+$file_name = join('_', $temp) . "_{$mtime}.xhprof";
+
+//  Generate full path
+$full_path = '/' . $root_path .'/'. $file_path .'/'. $file_name;
+$full_path = preg_replace('|/+|', '/', $full_path);
+
+//  Generate end-point path
+$temp = [];
+foreach( $config['link_path'] as $key => $value ){
+    if( $value ){
+        $temp[] = ${$key};
+    }
+}
+if( $temp ){
+    $link_path = $root_path . join('/', $temp) . "_{$mtime}.xhprof";
+    $link_path = preg_replace('|/+|', '/', $link_path);
 }
 
 //  Create save directory.
-if(!file_exists($dir) ){
+if(!file_exists($dir = dirname($full_path)) ){
     if(!mkdir($dir, 0777, true) ){
-        Notice("Could not create directory. ({$dir})");
+        OP()->Notice("Could not create file. ({$full_path})");
         return;
     }
 }
 
 //  Create xhprof.
-file_put_contents( $path, json_encode($data) );
+if(!file_put_contents( $full_path, json_encode($prof) )){
+    OP()->Notice("Could not create file. ({$full_path})");
+    return;
+}
 
-//  Create link file at URL.
-if( $link ?? null ){
+//  Show path to console
+if( OP()->MIME() === 'text/html' ){
+    echo "<script>console.log('{$full_path}')</script>";
+}
+
+//  Create link file of URL.
+if( $link_path ?? null ){
     //  ...
-    if(!$io = file_exists($link) ){
-        if(!$io = mkdir($link, 0777, true) ){
-            Notice("Could not create directory. ({$link})");
+    $dir = dirname($link_path);
+
+    //  Create directory.
+    if( $io = file_exists($dir) ){
+        if(!$io = is_dir( $dir) ){
+            OP()->Notice("This path is not a directory. ({$dir})");
+        }
+    }else{
+        if(!$io = mkdir($dir, 0777, true) ){
+            OP()->Notice("Could not make directory. ({$dir})");
         }
     }
 
-    //  ...
+    //  Create symbolic link.
     if( $io ){
-        symlink($path, $link.$file);
+        if( symlink($full_path, $link_path) ){
+            if( OP()->MIME() === 'text/html' ){
+                echo "<script>console.log('{$link_path}')</script>";
+            }
+        }
     }
 }
 
