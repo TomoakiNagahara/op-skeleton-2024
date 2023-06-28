c9e6b809594418252e70cf69567851826ef75a75
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2023-06-28 23:02:45 +0900

    Fix: example > index.phtml, html_pass_through.html, html_pass_through.css, html_pass_through.js | PHPDoc

--- example/html_pass_through.js
+++ example/html_pass_through.js
@@ -1,1 +1,17 @@
+<?php
+/** op-app-skeleton-2020:/html_pass_through.js
+ *
+ * @created   2019-12-06
+ * @version   1.0
+ * @package   op-app-skeleton-2020
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+?>
 console.log("<?php echo 'html-pass-through!!' ?>");
