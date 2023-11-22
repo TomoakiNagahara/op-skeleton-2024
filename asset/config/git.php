4bf7d3122f43717a2ce1e30a24c9ba6e6252bd2c
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-11-22 14:07:28 +0900

    New: asset > config > git.php | debug => false;

---
+++ asset/config/git.php
@@ -0,0 +1,25 @@
+<?php
+/** op-app-skeleton-2020:/asset/config/git.php
+ *
+ * @created		2023-11-21
+ * @version		1.0
+ * @package		op-app-skeleton-2020
+ * @author		Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright	Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** Return config array.
+ *
+ * @return	 array	 $config
+ */
+$config = [
+	'debug' => false,
+];
+
+//	...
+return $config;
