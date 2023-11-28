a9749abf54ef338c000d025c0a254c6fc1822265
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-11-28 12:35:50 +0900

    Fix: asset > bootstrap > app > config-admin.php | Check if set admin value.

--- asset/bootstrap/app/config-admin.php
+++ asset/bootstrap/app/config-admin.php
@@ -13,20 +13,25 @@
 //	...
 namespace OP;
 
-//	...
+//	Check if set admin.
+if( Env::_ADMIN_IP_ and Env::_ADMIN_MAIL_ ){
+	return;
+}
+
+//	Source file.
 $path = OP::MetaPath('asset:/config/admin.php');
 
-//	...
+//	Get and generate config.
 $file = file_get_contents($path);
 $file = Encode($file);
 $file = preg_replace(['|\/\*\* .+|','| \*.+|','| \*|','|\n/\*|','|\n\*/|'], '', $file);
 $file = preg_replace(['|//\t|','|\t|','|\n+|','|,\n|','|,\n\n\]|'], ['//  ','    ',"\n",",\n\n",",\n]"], $file);
 
-//	...
+//	Put config file.
 if(!file_put_contents(OP::MetaPath('asset:/config/').'_admin.php',  OP::Decode($file)) ){
 	echo 'Could not write to this file. (asset:/config/_admin.php)';
 	return false;
 }
 
-//	...
+//	True is success.
 return true;
