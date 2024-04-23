e4db7a87c3eb6da2d85e66d490600af038de78fc
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-04-23 11:57:41 +0900

    New: asset > config > html.php | lang, head > title

---
+++ asset/config/html.php
@@ -0,0 +1,30 @@
+<?php
+/** op-skeleton-2023:/asset/config/html.php
+ *
+ * @created		2023-12-27
+ * @version		1.0
+ * @package		op-skeleton-2023
+ * @author		Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright	Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** Return config array.
+ *
+ * @return    array        $config
+ */
+return [
+	'lang' => 'en',
+	'head' => [
+		'title' => 'The onepiece-framework app skeleton '._OP_APP_BRANCH_,
+	],
+];
