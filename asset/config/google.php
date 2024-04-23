78b56a25ffce91b1e4717cccea1f58ec3373ec35
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-04-23 11:56:42 +0900

    Doc: asset > config > google.php | PHPDoc

--- asset/config/google.php
+++ asset/config/google.php
@@ -1,11 +1,15 @@
 <?php
-/** op-app-skeleton-2020-nep:/asset/config/google.php
+/** op-skeleton-2020:/asset/config/google.php
  *
  * @created   2020-08-05
  * @version   1.0
- * @package   op-app-skeleton-2020-nep
+ * @package   op-skeleton-2020
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
 
+/** Return value is config array.
+ *
+ * @return array
+ */
 return [];
