906d2787a737548fd29a2afa9337805cb737d78d
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-05-13 01:26:12 +0900

    Doc: asset > check.php | PHPDoc

--- asset/check.php
+++ asset/check.php
@@ -1,9 +1,9 @@
 <?php
-/** op-app-skeleton-2020-nep:/asset/check.php
+/** op-skeleton-2020:/asset/check.php
  *
  * @created   2022-12-17
  * @version   1.0
- * @package   op-app-skeleton-2020-nep
+ * @package   op-skeleton-2020
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
