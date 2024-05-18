3df7c15054e352a5c08a95900197c310dd489637
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-05-18 14:02:21 +0900

    Doc: 404.php | PHPDoc

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <?php
-/** op-app-skeleton-2020-nep:/404.php
+/** op-skeleton-2020:/404.php
  *
  * @created   2018-10-30
  * @version   1.0
- * @package   op-app-skeleton-2020-nep
+ * @package   op-skeleton-2020
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
