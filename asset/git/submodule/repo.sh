34c8ab7b9b071443732eb5b0b49f9a3ce559ea4f
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-05-26 14:36:03 +0900

    Fix: asset > git > submodule > repo.sh | A GitHub user name is required now.

--- asset/git/submodule/repo.sh
+++ asset/git/submodule/repo.sh
@@ -1,11 +1,29 @@
 
+ ## op-app-skeleton-2020-nep:/asset/git/submodule/repo.sh
+ #
+ # @created    ????
+ # @version    1.0
+ # @package    op-app-skeleton-2020-nep
+ # @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ # @copyright  Tomoaki Nagahara All right reserved.
+ #
+
+# user name
+USER_NAME=${1}
+
+# Check argument.
+if [ -z "$USER_NAME" ]; then
+  echo 'Empty github accout name: sh asset/git/submodule/repo.sh [YOUR ACCOUNT NAME]'
+  exit 1
+fi
+
 # First, Change to local path.
-sh asset/git/submodule/local.sh
+sh asset/git/submodule/local.sh $USER_NAME
 if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# Copy
+# Backup
 cp .gitmodules .gitmodules_local
 
 # --> sudo port install realpath
@@ -13,6 +31,9 @@ HOME=$(realpath ~/)
 
 # Change to remote path from local path.
 sed -i -e "s|${HOME}/repo/|repo:~/repo/|g" .gitmodules
+
+# Backup
+cp .gitmodules .gitmodules_repo
 
 # Sync
 git submodule sync
