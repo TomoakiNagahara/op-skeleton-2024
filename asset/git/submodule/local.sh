2cf51ed7c9eb2dfce1fde1a339a75b6b4fbe57e9
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-05-26 14:30:33 +0900

    Fix: asset > git > submodule > local.sh | Change the first execute to github.sh.

--- asset/git/submodule/local.sh
+++ asset/git/submodule/local.sh
@@ -1,12 +1,36 @@
 
+ ## op-app-skeleton-2020-nep:/asset/git/submodule/local.sh
+ #
+ # @created    ????
+ # @version    1.0
+ # @package    op-app-skeleton-2020-nep
+ # @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ # @copyright  Tomoaki Nagahara All right reserved.
+ #
+
+# user name
+USER_NAME=${1}
+
+# Check argument.
+if [ -z "$USER_NAME" ]; then
+  echo 'Empty github accout name: sh asset/git/submodule/local.sh [YOUR ACCOUNT NAME]'
+  exit 1
+fi
+
+# First, Change to local path.
+sh asset/git/submodule/github.sh $USER_NAME
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
 # Copy backup.
-cp .gitmodules .gitmodules_original
+cp .gitmodules .gitmodules_github
 
 # --> sudo port install realpath
-HOME=$(realpath ~/)
+# HOME=$(realpath ~/)
 
 # Replace
-sed -i -e "s|https://github.com/onepiece-framework/|${HOME}/repo/op/|g" .gitmodules
+sed -i -e "s|https://github.com/${USER_NAME}/|${HOME}/repo/op/|g" .gitmodules
 sed -i -e "s|/unit-|/unit/|g"          .gitmodules # old path, Retained for compatibility.
 sed -i -e "s|/module-|/module/|g"      .gitmodules # old path, Retained for compatibility.
 sed -i -e "s|/layout-|/layout/|g"      .gitmodules # old path, Retained for compatibility.
