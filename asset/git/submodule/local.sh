ae8579b06124592005e2b08e08aa4a92c50692a7
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-06-02 19:12:49 +0900

    Fix: asset > git > submodule > local.sh | Not do rebase.

--- asset/git/submodule/local.sh
+++ asset/git/submodule/local.sh
@@ -53,7 +53,7 @@ git submodule sync
 git submodule foreach git fetch origin
 
 # Rebase to local/2022
-git submodule foreach git rebase origin/2022
+# git submodule foreach git rebase origin/2022
 
 # Delete garbage files.
 if [[ -e '.gitmodules-e' ]];then
