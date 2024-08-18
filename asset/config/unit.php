5bdb8e9fd521e6f35f9e22b9cdad35dad889e6c3
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-08-18 19:22:36 +0900

    2025: asset > config > unit.php | mapping

---
+++ asset/config/unit.php
@@ -0,0 +1,27 @@
+<?php
+/** op-skeleton-2024:/asset/config/unit.php
+ *
+ * @created   2024-06-08
+ * @version   1.0
+ * @package   op-skeleton-2024
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** Return config array.
+ *
+ * @return    array        $config
+ */
+$config = [
+	//	Mapping Unit to Unit name.
+	'mapping' => [
+		/*
+		'Form'     => 'form2',
+		'Database' => 'db2',
+		'ORM'      => 'orm2',
+		*/
+	],
+];
+
+//	...
+return $config;
