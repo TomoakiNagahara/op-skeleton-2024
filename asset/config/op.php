f0f91c105328e47df362cc5ae5e8e757514b363e
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-08-25 23:27:01 +0900

    2025: asset > config > op.php | define( _OP_APP_BRANCH_, 2024 --> 2025)

--- asset/config/op.php
+++ asset/config/op.php
@@ -1,9 +1,9 @@
 <?php
-/** op-app-skeleton-2020-nep:/asset/config/op.php
+/** op-skeleton-2020:/asset/config/op.php
  *
  * @created   2022-11-15
  * @version   1.0
- * @package   op-app-skeleton-2020-nep
+ * @package   op-skeleton-2020
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
@@ -19,7 +19,7 @@ declare(strict_types=1);
 namespace OP;
 
 //	onepiece-framework settings.
-define('_OP_APP_BRANCH_', '2024');
+define('_OP_APP_BRANCH_', '2025');
 
 //	...
 return [];
