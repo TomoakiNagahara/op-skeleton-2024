cd9b047fa4e6b0c7434b5054a90337a661b82933
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-10-20 01:49:08 +0900

    local: asset > config > cd2-MacStudioM1Ultra.php | New

---
+++ asset/config/cd2-MacStudioM1Ultra.php
@@ -0,0 +1,40 @@
+<?php
+/** op-skeleton-2024:/config/cd2-MacStudioM1Ultra.php
+ *
+ * @created    2023-01-02
+ * @moved      2023-02-05 from op-cd1
+ * @copied     2024-05-28 from op-cd2
+ * @copied     2024-10-18
+ * @version    1.0
+ * @package    op-cd2
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+require_once(__DIR__.'/op.php');
+
+//	...
+$user_name = 'TomoakiNagahara';
+$host_name = 'MacStudioM1Ultra';
+$repo_name = _OP_APP_BRANCH_;
+$repo_path = "~/repo/op/skeleton/{$repo_name}.git";
+
+//	...
+$config = [];
+$config['path']      = "/www/workspace/{$repo_name}/{$host_name}/";
+$config['origin']    = "{$repo_path}";
+$config['upstream']  = "{$host_name}:{$repo_path}";
+$config['github']    = "{$user_name}";
+$config['branch']    = "{$repo_name}";
+$config['gitmodules']=[
+	'origin'   => 'local',
+	'upstream' => $host_name,
+	'hostname' => $host_name,
+];
+$config['display']   = '0';
+$config['debug']     = '0';
+$config['version']   = '74, 80, 81, 82, 83'; // The PHP version to inspect.
+
+//	...
+return $config;
